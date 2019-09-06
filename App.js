cc92029c6c168f2482dc95bbc83596f830d9a94c
Jbwilliams84 <53539672+Jbwilliams84@users.noreply.github.com>
2019-09-06 11:05:29 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokegame />
+    </div>
+
+  );
+}
+
+export default App;
